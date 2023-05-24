4169cbb0b0cb9ea6f2629ff12e95f44435652b06
Gloubiboulgaah <gloubiboulga.sh@gmail.com>
2023-05-24 19:52:51 +0200

    upload du site

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var rect = $('#container')[0].getBoundingClientRect();
+var mouse = {x: 0, y: 0, moved: false};
+
+$("#container").mousemove(function(e) {
+  mouse.moved = true;
+  mouse.x = e.clientX - rect.left;
+  mouse.y = e.clientY - rect.top;
+});
+ 
+// Ticker event will be called on every frame
+TweenLite.ticker.addEventListener('tick', function(){
+  if (mouse.moved){    
+    parallaxIt("body", 0);
+    parallaxIt("h1",-50)
+    parallaxIt(".navbar",-50);
+    parallaxIt("#surf", -20);
+    parallaxIt("#surfBot", -15);
+    parallaxIt(".imageContainer",-15);
+    parallaxIt(".a_migna",-20);
+  }
+  mouse.moved = false;
+});
+
+function parallaxIt(target, movement) {
+  TweenMax.to(target, 0.3, {
+    x: (mouse.x - rect.width / 2) / rect.width * movement,
+    y: (mouse.y - rect.height / 2) / rect.height * movement
+  });
+}
+
+$(window).on('resize scroll', function(){
+  rect = $('#container')[0].getBoundingClientRect();
+})
